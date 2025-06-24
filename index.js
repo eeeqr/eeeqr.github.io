97078ce7d94d46d1d6a3006131fe05597157c1bf
eeeqr <123431332+eeeqr@users.noreply.github.com>
2025-06-23 19:57:02 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// Firebase configuration (replace with your actual config)
+const firebaseConfig = {
+    apiKey: "AIzaSyCzLPICo3Dh7ItN-o7SAFZeLSJ0qpcgXyU",
+    authDomain: "kylechat-8544f.firebaseapp.com",
+    databaseURL: "https://kylechat-8544f-default-rtdb.firebaseio.com",
+    projectId: "kylechat-8544f",
+    storageBucket: "kylechat-8544f.appspot.com",
+    messagingSenderId: "316422714408",
+    appId: "1:316422714408:web:c4392ceef018ee19e6b11d",
+    measurementId: "G-HZWMWB9B0C"
+};
+
